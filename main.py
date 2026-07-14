3ece5558345232210f1de7d2663e8fbb7dd0d179
DanielTheNext <280547582+DanielTheNext@users.noreply.github.com>
2026-07-13 20:24:30 -0500

    Implement basic Discord bot with commands
    Added a Discord bot with multiple commands including hello, heh, func, and math.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import discord
+from discord.ext import commands
+from bot_logic import *
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def func(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def math(ctx):
+    await ctx.send(practice_math())
+
+bot.run("Token Example")
