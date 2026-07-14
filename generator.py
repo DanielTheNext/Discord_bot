f715eb770f27136bff1f6b9253da358cc50521d6
DanielTheNext <280547582+DanielTheNext@users.noreply.github.com>
2026-07-13 20:26:01 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,12 @@
+import random
 
+def gen_key():
+    contraseña = ""
+
+    unitext =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+    for i in range(72):
+        pre_contraseña = random.choice(unitext)
+        contraseña += pre_contraseña
+
+    print(contraseña)
